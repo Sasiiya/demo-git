af1414bf3fe5a0368635d1129554c3922ba6bd90
sasi <pirratesofsparrow19@gmail.com>
2021-11-20 20:49:42 +0530

    comprehensive

--- case002/src/main/java/test/case002/App.java
+++ case002/src/main/java/test/case002/App.java
@@ -1,20 +1,25 @@
-package project;
+package test.case002;
 
+/**
+ * Hello world!
+ *
+ */
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-public class TestCase002 {
+public class App {
 
 	public static void main(String[] args) {
 		// To verify the user able to change the location
-		// 1. We’ll start with initializing the browser driver
+		// 1. Weâ€™ll start with initializing the browser driver
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SASI\\Downloads\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.eazydiner.com/");
 		
 		driver.findElement(By.cssSelector("#srchbar")).click();
